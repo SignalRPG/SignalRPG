44c9c9fddb60527f8bd806406f1c9945dd234865
Eric Butler <neptunecentury@gmail.com>
2014-07-31 18:17:02 -0400

    update

--- SignalRPG/SignalRPG/Scripts/game-map.js
+++ SignalRPG/SignalRPG/Scripts/game-map.js
@@ -1,9 +1,26 @@
 ﻿//map engine. loads a map json file and renders it
-function Map() {
+function Map(name) {
     var _self = this;
 
     //layers determine the order in which tiles are drawn. this is not necessarily the same
     //as the map generator layers. layers are built with the tile priorities; higher values being
     //drawn last.
     var _layers = [];
+
+    //get the map json file and build the map
+    function initialize() {
+        //get the json
+        $.getJSON('/resources/maps/' + name + '.json', {}, function (data) {
+            //json loaded
+
+            //step 1: get size of map
+
+            //setp 2: separate the layers out into logical layers to be drawn
+
+            //get all tiles with prioriry 0, and layer them first
+
+            //then, get all tiles with priority 1, and layer them last
+        });
+
+    }
 }
