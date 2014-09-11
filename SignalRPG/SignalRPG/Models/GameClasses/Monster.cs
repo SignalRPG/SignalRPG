7bb85f7b634ef842746b36b8c0749da7d0bf25a0
neptunecentury <neptunecentury@hotmail.com>
2014-09-11 18:07:54 -0400

    monster class

---
+++ SignalRPG/SignalRPG/Models/GameClasses/Monster.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using SignalRPG.Models.Hubs;
+using System.Threading;
+
+
+namespace SignalRPG.Models.GameClasses
+{
+    public class Monster : Sprite
+    {
+
+        public Monster()
+        {
+            MoveMonster();
+        }
+
+
+        #region Async
+        
+        private void MoveMonster()
+        {
+            //run an async method to move monster
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    //player's last position
+                    var charx = RandomEngine.Next(0, 21);
+                    var chary = RandomEngine.Next(0, 12);
+
+                    X = charx;
+                    Y = chary;
+
+                    GlobalHost.ConnectionManager.GetHubContext<GameHub>().Clients.All.moveMonster(ID, X, Y);
+
+                    Thread.Sleep(5000);
+                }
+            });
+
+
+        }
+
+        #endregion
+
+
+    }
+}
